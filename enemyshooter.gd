c0e2be5325eed103195fa3d6d9bd457a6cf499c5
Hectovargas <fefovargitas@gmail.com>
2024-03-14 11:33:08 -0600

    shooter terminado y conectado con la ruleta este titulo es bien largo porque si aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- enemyshooter.gd
+++ enemyshooter.gd
@@ -4,24 +4,13 @@ var vida = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y += 2
-	
+	position.y += 0.050
 	if vida == 0:
 		vida=1
 		$AnimatedSprite2D.play("new_animation")
 		await get_tree().create_timer(0.5).timeout
 		$AnimatedSprite2D.play("default")
-
-
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("arrow"):
-		print("colidekill")
-		vida = 0
-
 
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("arrow"):
@@ -29,7 +18,15 @@ func _on_area_2d_area_entered(area):
 			print("colidekill")
 			Global.correcto=true
 			vida = 0
-		elif Global.conshoo == 1 && is_in_group("enemys2")|| Global.conshoo == 2 && is_in_group("enemys2") || Global.conshoo == 4 && is_in_group("enemys2"):
+		elif Global.conshoo == 1 && is_in_group("enemys2"):
+			print("colidekill2")
+			Global.correcto=true
+			vida = 0
+		elif Global.conshoo == 2 && is_in_group("enemys2") :
+			print("colidekill2")
+			Global.correcto=true
+			vida = 0
+		elif Global.conshoo == 4 && is_in_group("enemys2"):
 			print("colidekill2")
 			Global.correcto=true
 			vida = 0
@@ -39,9 +36,9 @@ func _on_area_2d_area_entered(area):
 			print("colidekill3")
 		else:
 			Global.incorrecto=true
+		print("------------------------------")
+		print("pre"+str(Global.conshoo))
 		Global.conshoo+=1
-		if Global.conshoo==4:
-			Global.conshoo=0
-		print(Global.conshoo)
+		print("pos"+str(Global.conshoo))
 		
 			
--- shooter.gd
+++ shooter.gd
@@ -1,28 +1,67 @@
 extends Node2D
+var start = false
 var nodo
 var disparo = false
+var respondio = true
+var win = false
+var portal = false
 # Called when the node enters the scene tree for the first time.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $Marker2D.position.y<$TileMap/StaticBody2D.position.y:
-		$TileMap/Sprite2D.show()
-		Global.caida=true
-		await get_tree().create_timer(2).timeout
-		Global.caida=false
-		get_tree().change_scene_to_file("res://mazmorra con ruleta.tscn")
-	if Global.incorrecto == true:
-		Global.incorrecto = false
-		$TileMap/StaticBody2D.position.y += 10
-		$TileMap/StaticBody2D2.position.y += 10
-		$TileMap/StaticBody2D3.position.y += 10
-		$TileMap/StaticBody2D4.position.y += 10
+	if start == false:
+		$TileMap/StaticBody2D.position.y -= 0.050
+		$TileMap/StaticBody2D2.position.y -= 0.050
+		$TileMap/StaticBody2D3.position.y -= 0.050
+		$TileMap/StaticBody2D4.position.y -= 0.050
+	if portal==true:
+		$portal.show()
+	if Global.conshoo>=5:
+		$TextureRect.hide()
+		$TileMap/AnimatedSprite2D2.hide()
+		$TileMap/AnimatedSprite2D.queue_free()
+		$AreaDeSalida/AnimatedSprite2D.show()
+		Global.conshoo=0
+		$TileMap/StaticBody2D.queue_free()
+		$TileMap/StaticBody2D2.queue_free()
+		$TileMap/StaticBody2D3.queue_free()
+		$TileMap/StaticBody2D4.queue_free()
+		win = true
+	if win == false:
+		if $Marker2D.position.y<$TileMap/StaticBody2D.position.y:
+			$TileMap/Sprite2D.show()
+			Global.caida=true
+			$TileMap/StaticBody2D.hide()
+			$TileMap/StaticBody2D2.hide()
+			$TileMap/StaticBody2D3.hide()
+			$TileMap/StaticBody2D4.hide()
+			await get_tree().create_timer(2).timeout
+			Global.caida=false
+			get_tree().change_scene_to_file("res://mazmorra con ruleta.tscn")
 	if Global.correcto == true:
 		Global.correcto = false
-		$TileMap/StaticBody2D.position.y -= 10
-		$TileMap/StaticBody2D2.position.y -= 10
-		$TileMap/StaticBody2D3.position.y -= 10
-		$TileMap/StaticBody2D4.position.y -= 10
+		respondio=true
+		$TextureRect/Label.set_text("Correcto")
+		$TileMap/StaticBody2D.position.y -= 20
+		$TileMap/StaticBody2D2.position.y -= 20
+		$TileMap/StaticBody2D3.position.y -= 20
+		$TileMap/StaticBody2D4.position.y -= 20
+		if win==false:
+			await get_tree().create_timer(1).timeout
+			$TextureRect.hide()
+			$TileMap/AnimatedSprite2D2.show()
+	elif Global.incorrecto == true:
+		respondio=true
+		Global.incorrecto=false
+		$TileMap/StaticBody2D.position.y += 45
+		$TileMap/StaticBody2D2.position.y += 45
+		$TileMap/StaticBody2D3.position.y += 45
+		$TileMap/StaticBody2D4.position.y += 45
+		$TextureRect/Label.set_text("Incorrecto")
+		if win==false:
+			await get_tree().create_timer(1).timeout
+			$TextureRect.hide()
+			$TileMap/AnimatedSprite2D2.show()
 func _input(event):
 	if event.is_action_released("ui_focus_next"):
 			Global.flechando = true
@@ -33,6 +72,7 @@ func _input(event):
 	
 func _ready():
 	Global.progreso+=8
+	$portal/AnimatedSprite2D.play("default")
 	$flecha.add_to_group("arrow")
 	$TileMap/StaticBody2D.add_to_group("enemys")
 	$TileMap/StaticBody2D2.add_to_group("enemys2")
@@ -41,8 +81,35 @@ func _ready():
 
 
 func _on_area_2d_body_entered(body):
-	$TileMap/AnimatedSprite2D.play("normal")
-	$TileMap/AnimatedSprite2D2.hide()
+	start = true
+	if respondio == true:
+		respondio=false
+		$TileMap/AnimatedSprite2D.play("normal")
+		$TextureRect.show()
+		$Jugador.position.y-=40
+		match Global.conshoo:
+			
+			0:
+				$TextureRect/Label.set_text("Durante el renacimiento, el modelo de gobierno es uno de los siguientes")
+				await get_tree().create_timer(3).timeout
+				$TextureRect/Label.set_text("A) Monarquía absoluta. B) Tiranía republicana. C) Democracia participativa. D) Liberalismo político")
+			1:
+				$TextureRect/Label.set_text("De los siguientes acontecimientos, selecciones el que inicia el período moderno")
+				await get_tree().create_timer(3).timeout
+				$TextureRect/Label.set_text("A) Toma de Constantinopla. B) Tratado de paz de westfalia. C) Toma de la Bastilla. La ruta de la seda")
+			2:
+				$TextureRect/Label.set_text("Durante el siglo XV, la sociedad se estratifica en tres estamentos definidos")
+				await get_tree().create_timer(3).timeout
+				$TextureRect/Label.set_text("A) Clase media, baja y alta. B) nobleza, clero y estado llano. C) Artesanos, guardianes y gobernantes D) Feudales")
+			3:
+				$TextureRect/Label.set_text("Aparece el realismo politico, que se basaba en un orden establecido, explicación de un sistema y recomendaciones de como gobernar:")
+				await get_tree().create_timer(3).timeout
+				$TextureRect/Label.set_text("A) Tomas Moro. B) Jean Bodín. C) Nicolas Maquiavelo. D) Erasmo de Rotterda")
+			4:
+				$TextureRect/Label.set_text("Terminada la edad media, en el contexto de la politica resulta que:")
+				await get_tree().create_timer(3).timeout
+				$TextureRect/Label.set_text("A) La Iglesia resalta su poder. B) La Iglesia pierde el papel rector en la política. C) La Iglesia evangelica se posiciona en la politica. D) La politica desaparece")
+		$TileMap/AnimatedSprite2D2.hide()
 	
 
 
@@ -60,3 +127,17 @@ func _on_area_2d_5_body_entered(body):
 
 func _on_area_2d_5_area_entered(area):
 	pass # Replace with function body.
+
+
+func _on_portal_body_entered(body):
+	if portal == true:
+		get_tree().change_scene_to_file("res://mazmorra con ruleta.tscn")
+
+
+func _on_area_de_salida_body_entered(body):
+	if win == true:
+		$AreaDeSalida/AnimatedSprite2D.show()
+		portal = true
+		Global.maxvida+=1
+		Global.vida = Global.maxvida
+		$AreaDeSalida.position= Vector2(5000,5000)
